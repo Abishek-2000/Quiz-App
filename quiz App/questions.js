1764497268f27caa96535adceb1d9f7d508047b3
Abishek-2000 <159529407+Abishek-2000@users.noreply.github.com>
2024-11-06 06:55:22 +0530

    Add files via upload

---
+++ quiz App/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does JavaScript primarily run on??",
+    answer: "Web Browser",
+    options: [
+      "Web Server",
+      "Web Browser",
+      "Database",
+      "Operating System"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which event occurs when a user clicks on an HTML element?",
+    answer: "onclick event",
+    options: [
+      "onchange",
+      "onmouseclick",
+      "onclick event",
+      "onmouseover"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is Node.js?",
+    answer: "A runtime environment for executing JavaScript on the server",
+    options: [
+      "A JavaScript library for building UIs",
+      "A runtime environment for executing JavaScript on the server",
+      "A database for storing JavaScript objects",
+      " A framework for building websites"
+    ]
+  },
+];
